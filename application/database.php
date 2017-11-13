dea03f3ef76993d820a93f34b30c65bd6cded83e
18205962307 <514145034@qq.com>
2017-11-13 11:41:05 +0800

    修改数据库

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '120.25.149.181',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'zrrcms',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '1820596',
+    'password'        => 'root',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
